d1cd4b57ee8333da36e6991d9627f45516ddf134
sena-meucci <98917072+sena-meucci@users.noreply.github.com>
2022-03-03 01:08:53 +0100

    add stampa.cpp via upload

---
+++ stampa.cpp
@@ -0,0 +1,19 @@
+/*
+Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "n" dato in input dall'utente.
+
+NOME COGNOME DATA
+*/
+
+# include <iostream>
+using namespace std;
+int main()
+{
+  int i=10 ,n;
+  cout<<"Quante volte voi stampare la frase? ";
+  cin>>n;
+  while (i<n)
+  {
+   cout<<"Bada come la fuma"<<endl; 
+  }
+  system("pause");
+}
